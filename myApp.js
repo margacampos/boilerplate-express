b4d1b6a4c780fb5b730f31a098124132ea939273
Margarita Campos <80516949+margacampos@users.noreply.github.com>
2022-02-25 19:19:20 +0000

    first challenge

--- myApp.js
+++ myApp.js
@@ -2,7 +2,10 @@ var express = require('express');
 var app = express();
 console.log('Hello World');
 
-
+app.get('/', (req, res) => {
+  res.send('Successful response.');
+ console.log('Hello World');
+});
 
 
 
