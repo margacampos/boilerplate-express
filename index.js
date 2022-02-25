2ac8a5dde3740bca3cfc28dd9ae4298b9aa00441
Margarita Campos <80516949+margacampos@users.noreply.github.com>
2022-02-25 21:24:01 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
-var express = require('express');
+var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
-console.log('Hello World');
+console.log("Hello World");
+let absolutePath = __dirname + "/views/index.html";
 
-app.get('/', (req, res) => {
-  res.send('Hello Express');
- console.log('Hello World');
+app.use(function (req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(absolutePath);
 });
 
-app.listen(8080, () => console.log('Example app is listening on port 3000.'));
+app.get("/json", (req, res) => {
+  res.json({
+    message:
+      process.env.MESSAGE_STYLE !== "uppercase" ? "Hello json" : "HELLO JSON",
+    e: `${req.method} ${req.path} - ${req.ip}`,
+  });
+});
+
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.json({ time: req.time });
+  }
+);
+
+app.get('/:word/echo', (req, res)=>{
+  res.json({echo: req.params.word});
+});
+
+app.get('/name', (req,res)=>{
+  res.json({name: `${req.query.first} ${req.query.last}`})
+});
+
+app.post('/name', (req, res)=>{
+  res.json({name:`${req.body.first} ${req.body.last}`})
+});
+
+module.exports = app;
 
 
 
